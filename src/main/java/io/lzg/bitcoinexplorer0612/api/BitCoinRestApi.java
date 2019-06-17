ef1f38066454e66d751b6c7c00fe9d259852a2d5
LinKinggg <2604382918@qq.com>
2019-06-17 09:47:37 +0800

    commit bitcoinRestApi

---
+++ src/main/java/io/lzg/bitcoinexplorer0612/api/BitCoinRestApi.java
@@ -0,0 +1,34 @@
+package io.lzg.bitcoinexplorer0612.api;
+
+import com.alibaba.fastjson.JSONObject;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(name = "BitCoinRestApi", url = "http://localhost:18332")
+public interface BitCoinRestApi {
+
+    @GetMapping("/rest/chaininfo.json")
+    JSONObject getBlockChainInfo();
+
+    @GetMapping("/rest/block/notxdetails/{blockhash}.json")
+    JSONObject getBlockNoTxDetails(@PathVariable(value = "blockhash") String blockhash);
+
+    @GetMapping("/rest/tx/{txhash}.json")
+    JSONObject getBlockByHash(@PathVariable(value = "txhash") String txhash);
+
+    @GetMapping("/rest/headers/{blockhash}.json")
+    JSONObject getBlockheaders( @PathVariable(value = "blockhash")String blockhash);
+
+    @GetMapping("/rest/blockhashbyheight/{height}.json")
+    JSONObject getBlockByHeight(@PathVariable(value = "height") String height);
+
+    @GetMapping("/rest/getutxos/checkmempool/{blockhash}.json 2>/dev/null | json_pp")
+    JSONObject getUTXO(@PathVariable(value = "blockhash") String blockhash);
+
+    @GetMapping("/rest/mempool/info.json")
+    JSONObject getMempool();
+
+    @GetMapping("/rest/mempool/contents.json")
+    JSONObject getMempooltransactions();
+}
---
+++ src/main/java/io/lzg/bitcoinexplorer0612/controller/TempController.java
@@ -0,0 +1,31 @@
+package io.lzg.bitcoinexplorer0612.controller;
+
+import com.alibaba.fastjson.JSONObject;
+import io.lzg.bitcoinexplorer0612.api.BitCoinRestApi;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/temp")
+@EnableAutoConfiguration
+public class TempController {
+
+    @Autowired
+    private BitCoinRestApi bitcoinRestApi;
+
+    @GetMapping("/test")
+    public String test(){
+//        JSONObject blockChainInfo = bitcoinRestApi.getBlockChainInfo();
+        JSONObject blockNoTxDetails = bitcoinRestApi.getBlockNoTxDetails("00000000000003e333bb47e37002a1ee53f24f1f2613dbb57e848d128413ceb4");
+//        JSONObject blockheaders = bitcoinRestApi.getBlockheaders("00000000000003e333bb47e37002a1ee53f24f1f2613dbb57e848d128413ceb4");
+//        JSONObject mempool = bitcoinRestApi.getMempool();
+//        JSONObject mempooltransactions = bitcoinRestApi.getMempooltransactions();
+//        JSONObject blockByHeight = bitcoinRestApi.getBlockByHeight("15805");
+//        JSONObject blockByHash = bitcoinRestApi.getBlockByHash("00000000000003e333bb47e37002a1ee53f24f1f2613dbb57e848d128413ceb4");
+//        JSONObject utxo = bitcoinRestApi.getUTXO("00000000000003e333bb47e37002a1ee53f24f1f2613dbb57e848d128413ceb4");
+        return blockNoTxDetails.toJSONString();
+    }
+}
